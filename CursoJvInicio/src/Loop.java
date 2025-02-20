48a9cd5ae2fab261d18d25196b89b8a1b2ffd2f3
Italo-Ribeiro-Prates <italoribeiroprates@gmail.com>
2025-02-20 17:06:18 -0300

    first steps in JV2

---
+++ CursoJvInicio/src/Loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        
+        for (int i = 0; i < 3; i++) {
+
+        }
+
+
+    }
+}
