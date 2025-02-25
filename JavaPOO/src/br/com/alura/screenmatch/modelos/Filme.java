7e29b1ad1a63c5756dfbd998811a05a0d8414959
Italo-Ribeiro-Prates <italoribeiroprates424@gmail.com>
2025-02-25 15:24:09 -0300

    alteraçeos conforme as aulas foram avançando parei na aula evitando codigo duplicado

---
+++ JavaPOO/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme extends Titulo {
+        public String Diretor;
+
+        public String getDiretor() {
+                return Diretor;
+        }
+
+        public void setDiretor(String diretor) {
+                Diretor = diretor;
+        }
+}
+
