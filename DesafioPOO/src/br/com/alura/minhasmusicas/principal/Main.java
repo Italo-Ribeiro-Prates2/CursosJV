a9744fba006efd44f1ab33d73de1c3829bf08587
Italo-Ribeiro-Prates <italoribeiroprates@gmail.com>
2025-02-28 22:24:07 -0300

    começo do desafio de poo

---
+++ DesafioPOO/src/br/com/alura/minhasmusicas/principal/Main.java
@@ -0,0 +1,14 @@
+package br.com.alura.minhasmusicas.principal;
+
+import br.com.alura.minhasmusicas.modelos.Musicas;
+
+public class Main {
+    public static void main(String[] args) {
+        Musicas minhaMusicas = new Musicas();
+        minhaMusicas.setTitulo("Aerials");
+        minhaMusicas.setAlbum("Toxicity");
+        minhaMusicas.setCantor("System of a Down");
+
+
+    }
+}
