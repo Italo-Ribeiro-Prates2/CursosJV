97af7f773ae87ca5e69c9de29423d987a7d6bc09
Italo-Ribeiro-Prates <italoribeiroprates@gmail.com>
2025-03-03 19:34:27 -0300

    termino do desafio

--- DesafioPOO/src/br/com/alura/minhasmusicas/principal/Main.java
+++ DesafioPOO/src/br/com/alura/minhasmusicas/principal/Main.java
@@ -1,6 +1,8 @@
 package br.com.alura.minhasmusicas.principal;
 
+import br.com.alura.minhasmusicas.modelos.MinhasPreferidas;
 import br.com.alura.minhasmusicas.modelos.Musicas;
+import br.com.alura.minhasmusicas.modelos.podcast;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +11,30 @@ public class Main {
         minhaMusicas.setAlbum("Toxicity");
         minhaMusicas.setCantor("System of a Down");
 
+        for (int i = 0; i < 1000; i++) {
+            minhaMusicas.reproduzir();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusicas.curtir();
+        }
+
+        podcast meuPodcast = new podcast();
+        meuPodcast.setTitulo("Flow Podcast");
+        meuPodcast.setApresentador("Igor3k");
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.reproduzir();
+        }
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.curtir();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(minhaMusicas);
+        preferidas.inclui(meuPodcast);
+        
 
     }
 }
